cf09abdfed73400d25e3852e49ce3eace3c2af41
LucasJDove <86402904+Lucas-Uniqodo@users.noreply.github.com>
2021-07-06 09:36:53 +0100

    Arrays challenge task

---
+++ challenges/arrays.js
@@ -0,0 +1,12 @@
+const array = ['apples','pears','bananas'];
+
+const arrayLength = array.length;
+console.log('array length is ' + arrayLength);
+
+const firstElement = array[0]; // arrays start at index 0
+console.log('first element is ' + firstElement);
+
+// TODO - now try to print the second and third elements
+
+console.log('second element is ' + array[1]);
+console.log('second element is ' + array[2]);
